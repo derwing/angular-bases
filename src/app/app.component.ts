62884f736c4f1898747785cc7d688708d7a65462
Derwing Medina <derwing.medina@gmail.com>
2022-02-25 12:11:50 -0500

    switch statement for show/hide excercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  exercise1: boolean = true;
+  exercise2: boolean = true;
+  exercise3: boolean = true;
 
+  hideShowExercise(exerciseNumber: Number, showHide: boolean) {
+    switch (exerciseNumber) {
+      case 1:
+        this.exercise1 = showHide;
+        break;
 
+        case 2:
+          this.exercise2 = showHide;
+          break;
+
+          case 3:
+            this.exercise3 = showHide;
+            break;
+    
+      default:
+        break;
+    }
+  }
   
 }
 
